8c691dfe81cedba4b448d125073e73a3f16305ed
Ariel Naftali <arieln993@gmail.com>
2021-04-27 09:30:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const twilio = require('twilio')
 const express = require('express')
 const app = express()
 const port = 8080
-const lastCheck = '...'
+let lastCheck = '...'
 app.listen(port, () => {
     console.log('app is listening on port', port)
 })
